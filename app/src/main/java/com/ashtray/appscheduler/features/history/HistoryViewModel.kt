a3522a8b78172076981f198d8fffe24754bceb9c
Ashtray4715 <gobinda.paul.4715@gmail.com>
2022-02-23 23:01:51 +0600

    Removing a warning from history view model

--- app/src/main/java/com/ashtray/appscheduler/features/history/HistoryViewModel.kt
+++ app/src/main/java/com/ashtray/appscheduler/features/history/HistoryViewModel.kt
@@ -2,7 +2,6 @@ package com.ashtray.appscheduler.features.history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.ashtray.appscheduler.database.MyTaskEntity
 import com.ashtray.appscheduler.repository.MyRepository
 
