8ef5e76eb574ad820e622151f3b10e7c4ec5f615
Ashtray4715 <gobinda.paul.4715@gmail.com>
2022-02-23 19:28:31 +0600

    Showing all the tasks in ascending order

--- app/src/main/java/com/ashtray/appscheduler/database/MyTaskDao.kt
+++ app/src/main/java/com/ashtray/appscheduler/database/MyTaskDao.kt
@@ -8,10 +8,10 @@ import androidx.room.OnConflictStrategy
 
 @Dao
 interface MyTaskDao {
-    @Query("SELECT * FROM task_table WHERE is_done = 1")
+    @Query("SELECT * FROM task_table WHERE is_done = 1 ORDER BY start_time ASC")
     fun getAllTheCompletedTask(): LiveData<List<MyTaskEntity>>
 
-    @Query("SELECT * FROM task_table WHERE is_done = 0")
+    @Query("SELECT * FROM task_table WHERE is_done = 0 ORDER BY start_time ASC")
     fun getAllTheRemainingTask(): LiveData<List<MyTaskEntity>>
 
     @Insert(onConflict = OnConflictStrategy.ABORT)
